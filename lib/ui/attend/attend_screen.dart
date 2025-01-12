84e64b8ba043dfeea24e8a2a85229c217c571f9a
Setianing Budi <setianingbudi2@gmail.com>
2025-01-12 10:21:44 +0700

    create a file placeholder for each main screen

---
+++ lib/ui/attend/attend_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AttendScreeen extends StatelessWidget {
+  const AttendScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
